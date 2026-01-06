29a1dd4466ac9c147c3e43bad6c2c5f44d093e9d
CaelOConnor <coconnor10@students.stonehill.edu>
2026-01-05 19:01:36 -0500

    updated userinput methods for edge cases

--- userinput.py
+++ userinput.py
@@ -60,16 +60,38 @@ class Solver:
     # guess bassed on on info from prev info
     def next_guess(self, guess_letters):
         possible_words = self.words.copy()
+        greens = set()
+        yellows = set()
+
+        for ltr in guess_letters:
+            let = ltr.let.lower()
+            if ltr.color == "green":
+                greens.add(let)
+            elif ltr.color == "yellow":
+                yellows.add(let)
 
         for ltr in guess_letters:
             let, col, pos = ltr.let.lower(), ltr.color, ltr.pos
 
-            if col == "gray": # fix latter for words where a letter is both gray and green or yellow
-                possible_words = possible_words[~possible_words.str.contains(let)] # words = word[not words that contains that letter]
+            if col == "gray": # sub cases ofr gray because of duplicate letter edge cases
+                if let not in greens and let not in yellows: # remove all words with the gray letter
+                    possible_words = possible_words[~possible_words.str.contains(let)] # words = word[not words that contains that letter]
+                elif let in greens and let not in yellows: # words all words except ones that have the green letter
+                    green_positions = [ltr.pos for ltr in guess_letters if ltr.let.lower() == let and ltr.color == "green"]
+                    mask = pd.Series(False, index=possible_words.index)
+                    for pos_idx in green_positions:
+                        mask |= possible_words.str[pos_idx] == let
+                    possible_words = possible_words[mask]
+                elif let not in greens and let in yellows: # remove all words that have that letter in the gray spot and yellow but not the others
+                    mask = possible_words.str[pos] != let
+                    yellow_positions = [ltr.pos for ltr in guess_letters if ltr.let.lower() == let and ltr.color == "yellow"]
+                    yellow_mask = pd.Series(False, index=possible_words.index)
+                    for yp in yellow_positions:
+                        yellow_mask |= possible_words.str[yp] != let
+                    possible_words = possible_words[mask & yellow_mask] # combine masks
 
             elif col == "green":
                 possible_words = possible_words[possible_words.str[pos] == let] # contains the letters
-
             elif col == "yellow":
                 mask = possible_words.str.contains(let) # contains the ltters but not in that spot
                 mask &= possible_words.str[pos] != let
@@ -90,7 +112,12 @@ def main():
     while guess_counter <= 5:
         user_guess = solver.get_info()
         #print(user_guess)
+        finished = input("Did you guess the word correctly? (y/n): ").strip().lower()
+        if finished == "y":
+            print(f"Congratulations! You solved it in {guess_counter} guesses.")
+            break
         next_guess_to_use = solver.next_guess(user_guess)
+        guess_counter += 1
 
 if __name__ == "__main__":
     main()
